4667491bd3c31a6cad59ea3383b7db801e07a016
Nicholas Clark <nick4jesus@gmail.com>
2025-07-15 10:04:24 -0500

    Update Montana Net. W7XY is a silent key.

--- src/routes/net/mt-net.svelte
+++ src/routes/net/mt-net.svelte
@@ -5,14 +5,14 @@
     </strong>
   </p>
   <p class="auto-style9">
-    This net has changed to an informal net called by Doug K7YD on Sunday morning on 3.880 Frequency
-    at 8:00 A.M. Mountain Standard Time. If he is not there then someone should take a turn and call
-    the net. This net is just to keep everyone informed on to what is happening and information
-    about items pertaining to Amateur Radio in the area. As practice W7XY takes weather reports if
-    he is present and turns them into the Weather Service in Missoula, Billings, Great Falls and
-    Glasgow. They really appreciate this and tell us Thanks for doing this. If you have any
-    information to share or news of silent keys let the Net Control know and give it to the net.
-    This is a fun net and usually lasts almost an hour so join in on the fun.
+    This net is an informal net called by Les N7CMJ or Kevin W7KRM on Sunday
+    morning on 3.880 Frequency at 8:00 A.M. Mountain Standard Time. If he is
+    not there then someone should take a turn and call the net. This net is
+    just to keep everyone informed on to what is happening and information
+    about items pertaining to Amateur Radio in the area. If you have any
+    information to share or news of silent keys let the Net Control know and
+    give it to the net. This is a fun net and usually lasts almost an hour
+    so join in on the fun.
   </p>
   <img alt="Have fun" height="126" src="/img/antenna-1.gif" width="120" />
 </div>
